150f6243ea7e10a177a067639e377dc0ae20edd3
nickolas <nikolay.svystun@rokitteck.ua>
2016-02-12 14:26:22 +0200

    back

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = createLayout;
-module.exports.simulator = require('ngraph.physics.simulator.v2');
+module.exports.simulator = require('ngraph.physics.simulator');
 
 /**
  * Creates force based layout for a given graph.
@@ -17,7 +17,7 @@ function createLayout(graph, physicsSettings) {
     throw new Error('Graph structure cannot be undefined');
   }
 
-  var createSimulator = require('ngraph.physics.simulator.v2');
+  var createSimulator = require('ngraph.physics.simulator');
   var physicsSimulator = createSimulator(physicsSettings);
 
   var nodeBodies = typeof Object.create === 'function' ? Object.create(null) : {};
@@ -250,6 +250,7 @@ function createLayout(graph, physicsSettings) {
       if (to) updateBodyMass(to.id);
 
       delete springs[link.id];
+
 
       physicsSimulator.removeSpring(spring);
     }
